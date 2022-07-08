7a9c9e6d38ba2adf2dd5bf49c41b9a9c34bdf8c7
Tzukuyomii <97518242+Tzukuyomii@users.noreply.github.com>
2022-07-08 10:01:32 +0200

    Add and configure eslint with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    overrides: [
+        {
+            files: ["*.ts"],
+            rules: {
+                "no-undef": "off",
+            },
+        },
+        {
+            files: ["*.test.ts"],
+            env: {
+                node: true,
+                jest: true,
+            },
+        },
+    ],
+    rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+    },
+};
