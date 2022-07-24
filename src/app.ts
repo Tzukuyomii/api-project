20f763a037649c8593de5402ee6ee868ece53758
Tzukuyomii <97518242+Tzukuyomii@users.noreply.github.com>
2022-07-24 13:22:26 +0200

    Add delete planets id route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import "express-async-errors";
 import { PrismaClient } from "@prisma/client";
 
@@ -50,6 +50,42 @@ app.post(
         response.status(201).json(planet);
     }
 );
+
+app.put(
+    "/planets/:id(\\d+)",
+    validate({ body: planetSchema }),
+    async (request, response, next) => {
+        const planetId = Number(request.params.id);
+        const planetData: PlanetData = request.body;
+
+        try {
+            const planet = await prisma.planet.update({
+                where: { id: planetId },
+                data: planetData,
+            });
+
+            response.status(200).json(planet);
+        } catch (error) {
+            response.status(404);
+            next(`Cannot PUT /planets/${planetId}`);
+        }
+    }
+);
+
+app.delete("/planets/:id(\\d+)", async (request, response, next) => {
+    const planetId = Number(request.params.id);
+
+    try {
+        await prisma.planet.delete({
+            where: { id: planetId },
+        });
+
+        response.status(204).end();
+    } catch (error) {
+        response.status(404);
+        next(`Cannot DELETE /planets/${planetId}`);
+    }
+});
 
 app.use(validationErrorMiddleware);
 
